56943fad584e60d39a95314e0e1917bcea729f2d
Cameron Smart <smartcameron@gmail.com>
2024-04-18 20:21:58 -0700

    Reusable radio selecting function
    This makes the steps definition file much shorter, but ids have to be supplied by the feature file

--- features/steps/step_definitions.py
+++ features/steps/step_definitions.py
@@ -16,87 +16,27 @@ def wait_for_element(context, element_locator, by=By.CSS_SELECTOR, timeout=10):
         EC.visibility_of_element_located((by, element_locator))
     )
 
-# User navigates to the data store site's removal page
-
 @given('I am on the data store site\'s removal page')
 def step_impl(context: Context):
     context.browser = webdriver.Chrome()
     context.browser.get("https://support.google.com/websearch/answer/9673730")
 
-@then('I pause on a page for "{seconds}" seconds')
-def step_impl(context, seconds):
-    time.sleep(int(seconds))
-
-@then('I click the "Start removal request" button')
+@then('I click the "Start removal request" link')
 def step_impl(context: Context):
     button = context.browser.find_element(By.LINK_TEXT, "Start removal request")
     button.click()
-
-# User selects the type of content they want to remove
-
-@then('I select "Content contains nudity or sexual material"')
-def step_impl(context):
-    wait_for_element(context, "label[for='user_select--explicit_content']")
-    label_for_explicit_content = context.browser.find_element(By.CSS_SELECTOR, "label[for='user_select--explicit_content']")
-    label_for_explicit_content.click()
-
-@then('I select "Content contains your personal information"')
-def step_impl(context):
-    wait_for_element(context, "label[for='user_select--nonexplicit_personal_pii']")
-    label_for_personal_info = context.browser.find_element(By.CSS_SELECTOR, "label[for='user_select--nonexplicit_personal_pii']")
-    label_for_personal_info.click()
-
-@then('I select "Content is on a site with exploitative removal practices"')
-def step_impl(context):
-    wait_for_element(context, "label[for='user_select--predatory_removals']")
-    label_for_exploitative_practices = context.browser.find_element(By.CSS_SELECTOR, "label[for='user_select--predatory_removals']")
-    label_for_exploitative_practices.click()
-
-@then('I select "Content shows a person under 18"')
-def step_impl(context):
-    wait_for_element(context, "label[for='user_select--nonexplicit_minor']")
-    label_for_minor_content = context.browser.find_element(By.CSS_SELECTOR, "label[for='user_select--nonexplicit_minor']")
-    label_for_minor_content.click()
 
 @then('I click the "Next" button')
 def step_impl(context):
     next_button = context.browser.find_element(By.XPATH, "//button[contains(@class, 'next-button') and text()='Next']")
     next_button.click()
 
-# User selects the type of personal information they want to remove
+@then('I select the radio button with id "{radio_id}"')
+def step_impl(context, radio_id):
+    wait_for_element(context, f"label[for='{radio_id}']")
+    radio_label = context.browser.find_element(By.CSS_SELECTOR, f"label[for='{radio_id}']")
+    radio_label.click()
 
-@then('I select "Address, phone number, and/or e-mail address"')
-def step_impl(context):
-    wait_for_element(context, "label[for='pii_field--contact']")
-    label = context.browser.find_element(By.CSS_SELECTOR, "label[for='pii_field--contact']")
-    label.click()
-
-@then('I select "Confidential government identification numbers"')
-def step_impl(context):
-    wait_for_element(context, "label[for='pii_field--govt']")
-    label = context.browser.find_element(By.CSS_SELECTOR, "label[for='pii_field--govt']")
-    label.click()
-
-@then('I select "Bank account or credit card number"')
-def step_impl(context):
-    wait_for_element(context, "label[for='pii_field--bank']")
-    label = context.browser.find_element(By.CSS_SELECTOR, "label[for='pii_field--bank']")
-    label.click()
-
-@then('I select "Images of a handwritten signature or an ID doc"')
-def step_impl(context):
-    wait_for_element(context, "label[for='pii_field--signature']")
-    label = context.browser.find_element(By.CSS_SELECTOR, "label[for='pii_field--signature']")
-    label.click()
-
-@then('I select "Highly personal, restricted, and official records"')
-def step_impl(context):
-    wait_for_element(context, "label[for='pii_field--official']")
-    label = context.browser.find_element(By.CSS_SELECTOR, "label[for='pii_field--medical']")
-    label.click()
-
-@then('I select "Confidential login credentials"')
-def step_impl(context):
-    wait_for_element(context, "label[for='pii_field--login']")
-    label = context.browser.find_element(By.CSS_SELECTOR, "label[for='pii_field--login']")
-    label.click()
+@then('I pause on a page for "{seconds}" seconds')
+def step_impl(context, seconds):
+    time.sleep(int(seconds))
